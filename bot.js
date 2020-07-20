0e54c134c773f7252598091ccb3bd9143034dd6e
MerysPiegay <merys.piegay@gmail.com>
2020-07-20 18:48:10 +0200

    bot score

--- bot.js
+++ bot.js
@@ -1,29 +1,49 @@
-const Discord = require('discord.js');
-
+const Discord = require("discord.js");
 const client = new Discord.Client();
 
- 
+const SQLite = require("better-sqlite3");
+const sql = new SQLite('./scores.sqlite');
 
-client.on('ready', () => {
+client.on("ready", () => {
+  // Check if the table "points" exists.
+  const table = sql.prepare("SELECT count(*) FROM sqlite_master WHERE type='table' AND name = 'scores';").get();
+  if (!table['count(*)']) {
+    // If the table isn't there, create it and setup the database correctly.
+    sql.prepare("CREATE TABLE scores (id TEXT PRIMARY KEY, user TEXT, guild TEXT, points INTEGER, level INTEGER);").run();
+    // Ensure that the "id" row is always unique and indexed.
+    sql.prepare("CREATE UNIQUE INDEX idx_scores_id ON scores (id);").run();
+    sql.pragma("synchronous = 1");
+    sql.pragma("journal_mode = wal");
+  }
 
-	    console.log('I am ready!');
-
+  // And then we have two prepared statements to get and set the score data.
+  client.getScore = sql.prepare("SELECT * FROM scores WHERE user = ? AND guild = ?");
+  client.setScore = sql.prepare("INSERT OR REPLACE INTO scores (id, user, guild, points, level) VALUES (@id, @user, @guild, @points, @level);");
 });
 
- 
+client.on("message", message => {
+  if (message.author.bot) return;
+  let score;
+  if (message.guild) {
+    score = client.getScore.get(message.author.id, message.guild.id);
+    if (!score) {
+      score = { id: `${message.guild.id}-${message.author.id}`, user: message.author.id, guild: message.guild.id, points: 0, level: 1 }
+    }
+    score.points++;
+    const curLevel = Math.floor(0.1 * Math.sqrt(score.points));
+    if(score.level < curLevel) {
+      score.level++;
+      message.reply(`You've leveled up to level **${curLevel}**! Ain't that dandy?`);
+    }
+    client.setScore.run(score);
+  }
+  if (message.content.indexOf(config.prefix) !== 0) return;
 
-client.on('message', message => {
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
 
-	    if (message.content === 'ping') {
-
-		           message.reply('pong');
-
-		           }
-
+  // Command-specific code here!
 });
 
- 
 
-// THIS  MUST  BE  THIS  WAY
-//
 client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
