f4947f239462397bc98a44a46a1b77b3f18d0cdf
MerysPiegay <merys.piegay@gmail.com>
2020-07-20 23:18:33 +0200

    reset

--- index.js
+++ index.js
@@ -10,11 +10,6 @@ const bot = new commando.Client({
     owner:['Robière#7240']
 });
 
-//Register Commands
-bot.registry.registerGroup('count', 'count');
-bot.registry.registerGroup('math', 'math');
-bot.registry.registerDefaults();
-bot.registry.registerCommandsIn(__dirname + '/commands')
 bot.login('taAdGSaeG9W1FMgwz2_XQhBscqJ51YaI');
 
 
@@ -33,38 +28,3 @@ bot.on('ready', () => {
     });
 });
 
-//When a message is sent
-bot.on('message', (message) => {
-    //Not send by a bot and not a command
-    if(message.author.bot == false && (message.content.startsWith(prefix) == false) ){
-        //Connected to database
-        pool.connect( (err, client, done) => {
-            //Increment users count by 1
-            client.query('update users set count = count + 1 where id = $1',
-            [message.author.id], (err, result) => {
-
-                done(err);
-                //If user not in the database add them
-                if (result.rowCount == 0){
-                    client.query('insert into users (id, name, count) values ($1, $2, 1)',
-                    [message.author.id, message.author.username], (err, result) => {
-                        done(err);
-                        console.log(result.rowCount);
-                    });
-                }
-            });
-        });
-    }
-});
-
-//User changes nickname
-bot.on('guildMemberUpdate', (oldguy, newguy) => {
-    pool.connect( (err, client, done) => {
-        //Update display name to new nickname
-        client.query('update users set name = $1 where id = $2',
-        [newguy.displayName, newguy.user.id], (err, result) => {
-            done(err);
-
-        });
-    });
-});
