0e2a6f9394d8cb2b253d2d79695ea864b9926484
MerysPiegay <merys.piegay@gmail.com>
2020-07-20 19:16:45 +0200

    correctif

--- bot.js
+++ bot.js
@@ -43,7 +43,49 @@ client.on("message", message => {
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
 
-  // Command-specific code here!
+  if(command === "give") {
+  // Limited to guild owner - adjust to your own preference!
+  if(!message.author.id === message.guild.owner) return message.reply("You're not the boss of me, you can't do that!");
+
+  const user = message.mentions.users.first() || client.users.get(args[0]);
+  if(!user) return message.reply("You must mention someone or give their ID!");
+
+  const pointsToAdd = parseInt(args[1], 10);
+  if(!pointsToAdd) return message.reply("You didn't tell me how many points to give...")
+
+  // Get their current points.
+  let userscore = client.getScore.get(user.id, message.guild.id);
+  // It's possible to give points to a user we haven't seen, so we need to initiate defaults here too!
+  if (!userscore) {
+    userscore = { id: `${message.guild.id}-${user.id}`, user: user.id, guild: message.guild.id, points: 0, level: 1 }
+  }
+  userscore.points += pointsToAdd;
+
+  // We also want to update their level (but we won't notify them if it changes)
+  let userLevel = Math.floor(0.1 * Math.sqrt(score.points));
+  userscore.level = userLevel;
+
+  // And we save it!
+  client.setScore.run(userscore);
+
+  return message.channel.send(`${user.tag} has received ${pointsToAdd} points and now stands at ${userscore.points} points.`);
+}
+
+if(command === "leaderboard") {
+  const top10 = sql.prepare("SELECT * FROM scores WHERE guild = ? ORDER BY points DESC LIMIT 10;").all(message.guild.id);
+
+    // Now shake it and show it! (as a nice embed, too!)
+  const embed = new Discord.RichEmbed()
+    .setTitle("Leaderboard")
+    .setAuthor(client.user.username, client.user.avatarURL)
+    .setDescription("Our top 10 points leaders!")
+    .setColor(0x00AE86);
+
+  for(const data of top10) {
+    embed.addField(client.users.get(data.user).tag, `${data.points} points (level ${data.level})`);
+  }
+  return message.channel.send({embed});
+}
 });
 
 
