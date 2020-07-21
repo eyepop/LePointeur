c494ea717b1d6842130d913450988cdb656055d3
RadioCom A5RHONE <eyepop.eyepop@gmail.com>
2020-07-21 20:41:57 +0200

    ok ok

--- bot.js
+++ bot.js
@@ -2,4 +2,4 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
-const channel=client.channels.find("name","la-salle-des-points");
+client.channels.cache.find(channel => channel.name === 'la-salle-des-points').send("ok ok ");
