962c2f029ebc0eaff36534bfed6d402b84db9ed3
RadioCom A5RHONE <eyepop.eyepop@gmail.com>
2020-07-21 21:39:55 +0200

    ok ok

--- bot.js
+++ bot.js
@@ -2,12 +2,13 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on('ready', () => {
-client.channels.cache.forEach(logMapElements);
-	const channel=client.channels.cache.get("735193960783413351");
-	channel.send("okok");
+	client.channels.cache.forEach(logMapElements);
+	const chanPoints=client.channels.cache.get("735193960783413351");
+	const chanGen=client.channels.cache.get("690970175956189209");
+	chanPoints.send("okok");
 });
 client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
 
 function logMapElements(value, key, map) {
-  console.log('m['+key+'] = '+value);
+	console.log('m['+key+'] = '+value);
 }
