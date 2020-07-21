b244c5c70015975195d5b9abd68db1d47ed2f1e5
RadioCom A5RHONE <eyepop.eyepop@gmail.com>
2020-07-21 21:51:26 +0200

    ok ok

--- bot.js
+++ bot.js
@@ -5,9 +5,19 @@ client.on('ready', () => {
 	client.channels.cache.forEach(logMapElements);
 	const chanPoints=client.channels.cache.get("735193960783413351");
 	const chanGen=client.channels.cache.get("690970175956189209");
+	wipeChan(chanPoints);
 	chanPoints.send("okok");
 });
 client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
+
+
+function wipeChan(chan){
+	chan.messages.fetch({limit: 1})
+  .then(messages => {
+    messages.first().delete();
+  });
+
+}
 
 function logMapElements(value, key, map) {
 	console.log('m['+key+'] = '+value);
