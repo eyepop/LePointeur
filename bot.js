d0bf8e26e488b7cd4331c66a05946a82cd108ed5
RadioCom A5RHONE <eyepop.eyepop@gmail.com>
2020-07-21 20:04:35 +0200

    changement d'idee

--- bot.js
+++ bot.js
@@ -1,5 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
+const channel = client.channels.find('la-salle-des-points', channelName);
+channel.send("turlututu");
 
 client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
