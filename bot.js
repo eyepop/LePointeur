79d451d0e53f165839af8fa411ef2bf21c9a76dd
RadioCom A5RHONE <eyepop.eyepop@gmail.com>
2020-07-21 21:03:03 +0200

    ok ok

--- bot.js
+++ bot.js
@@ -2,6 +2,6 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on('ready', client => {
-	client.channels.get('735193960783413351').send('Hello here!');
+	console.log(client.channels);
 });
 client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
