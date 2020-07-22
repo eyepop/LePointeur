913ec6a067d181a3150859beef8a66537062fb57
RadioCom A5RHONE <eyepop.eyepop@gmail.com>
2020-07-22 08:25:05 +0200

    ok ok

--- bot.js
+++ bot.js
@@ -30,7 +30,7 @@ client.on('message', msg => {
 	}
 });
 
-const jsonex="{ id : 172348173166051328; username : Robière ; points :0}";
+const jsonex='{"id" , "172348173166051328", "username" , "Robière" , "points" ,"0"}';
 client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
 
 function addPoints(bot,id,username,nb,chan){
