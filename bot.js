a8c9fea7258682366cf8097ede8b271baae82f78
RadioCom A5RHONE <eyepop.eyepop@gmail.com>
2020-07-22 09:20:37 +0200

    ok ok

--- bot.js
+++ bot.js
@@ -27,7 +27,7 @@ client.on('message', msg => {
 				}
 			}
 		}
-	msg.reply(nb+"->"+dest.replace("/[<>]/gi",""));
+	msg.reply(nb+"->"+dest.replace("/[<>@]/gi",""));
 	console.log(nb+"->"+dest.replace("/[<!>]/gi",""));
 	}
 });
