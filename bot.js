16616307175577cd3ee1058b0883a7b996f819e0
RadioCom A5RHONE <eyepop.eyepop@gmail.com>
2020-07-22 11:29:53 +0200

    ok ok

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ client.on('ready', () => {
 	const chanGen=client.channels.cache.get("690970175956189209");
 	const chanTest=client.channels.cache.get("692075989026734090");
 	//wipeChan(chanPoints);
-	chan.messages.fetch().then(nbMessages+=1);
+	chanPoints.messages.fetch().then(nbMessages+=1);
 	console.log("empty "+nbMessages);
 	if(chanPoints.lastMessage==null){
 		//	chanGen.members.forEach(member => initPoints(member.user.bot,member.id,member.user.username,0,chanPoints));
