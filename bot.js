af68f21fe5f6039b3342968021488657eb9f461b
RadioCom A5RHONE <eyepop.eyepop@gmail.com>
2020-07-22 17:42:49 +0200

    ok ok

--- bot.js
+++ bot.js
@@ -51,7 +51,7 @@ function addPoints(bot,id,username,nb,chan){
 			messages.forEach(function(message){ 
 				if(message.content.includes('{"id" : "'+id+'",')){
 					var nb =parseFloat(nb)+parseFloat(parseMsg(message.content).scores.points);
-					console.log(parseMsg(message.content).scores);
+					console.log(parseMsg(message.content).scores.points);
 					const jsonForm='{"id" : "'+id+'", "username" : "'+username+'" , "scores":{"points" :'+nb+'}}';
 					console.log(message.content);
 					message.edit(jsonForm).then(console.log(message.content));
